f1deaeb7cd182c2adbe3a70bed4121bfd1404b65
r357 <32488308+r357@users.noreply.github.com>
2017-10-17 20:45:08 +0200

    Original GE1 py

--- General_Equilibrium/Dynamic_GE1.py
+++ General_Equilibrium/Dynamic_GE1.py
@@ -1,39 +1,43 @@
-# TWO PERIOD GENERAL EQUILIBRIUM MODEL # WITH TWO TYPES OF AGENTS IN TERMS OF
+# TWO PERIOD GENERAL EQUILIBRIUM MODEL 
+# WITH TWO TYPES OF AGENTS IN TERMS OF
 # ENDOWMENTS AND PREFERENCES (DISCOUNT RATE)
-
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # KEY PARAMETERS
+
 # PREFERENCE PARAMETERS
-rho1 = np.arange(0.0,0.2,0.02) # discount rate 1 
-rho2 = np.arange(0.0,0.2,0.02) # discount rate 2
+    
+rho1 = np.arange(0.02,0.2,0.02) # discount rate 1 
+rho2 = np.arange(0.02,0.2,0.02) # discount rate 2
 
+# ENDOWMENTS
+# NOTATION 12 - denotes agent 1 period 2 (see consumption function below)
 
-# ENDOWMENTS# NOTATION 12 - denotes agent 1 period 2 (see consumption function below); (minimum,maximum,step)
 Q11 = np.arange(1.0,2.0,0.05)
 Q12 = np.arange(1.0,2.0,0.05)
 Q21 = np.arange(1.0,2.0,0.05)
 Q22 = np.arange(1.0,2.0,0.05)
 
-
 # ix function for indexing shapes
+
 Q11x,Q12x,Q21x,Q22x,rho1x,rho2x = np.ix_(Q11,Q12,Q21,Q22,rho1,rho2)
 
-
 # INTEREST RATE - SOLUTION TO GE
-Q11x.shape, Q12x.shape, Q21x.shape, Q22x.shape, rho1x.shape, rho2x.shape
-r  = -1 + ((1+rho1x)/(2+rho1x)*Q12x+(1+rho2x)/(2+rho2x)*Q22x)/(Q11x/(2+rho1x)+(Q21x/(2+rho2x))
 
-C11 = (1+rho1x)/((2+rho1x)*(Q11x+Q12x/(1+r)))
+Q11x.shape, Q12x.shape, Q21x.shape, Q22x.shape, rho1x.shape, rho2x.shape
+r  = -1 + ((1+rho1x)/(2+rho1x)*Q12x+(1+rho2x)/(2+rho2x)*Q22x)/(Q11x/(2+rho1x)+Q21x/(2+rho2x))
+
+C11 = (1+rho1x)/(2+rho1x)*(Q11x+Q12x/(1+r))
 C12 = (1+r)/(2+rho1x)*(Q11x+Q12x/(1+r))
 C21 = (1+rho2x)/(2+rho2x)*(Q21x+Q22x/(1+r))
 C22 = (1+r)/(2+rho2x)*(Q21x+Q22x/(1+r))
 
 
 # PLOT 1: VARIATION OF ENDOWMENT of Agent Type 1 in period 1
+
+
 r_1 = r[:,0,0,0,0,0]
 C11_1 = C11[:,0,0,0,0,0]
 C12_1 = C12[:,0,0,0,0,0]
@@ -57,8 +61,8 @@ ax1.set_xlabel('Endowment $Q_{1}^{1}$')
 ax1.set_ylabel('Consumption ')
 #fig2.savefig('C:/aaaCourses/AdvMacro/aaaHomeworks/2017/Python/fig2_C_Q11.png')   # save the figure to file
 
-
 # PLOT 2: VARIATION OF ENDOWMENT of Agent Type 1 in period 2
+
 r_2   =   r[0,:,0,0,0,0]
 C11_2 = C11[0,:,0,0,0,0]
 C12_2 = C12[0,:,0,0,0,0]
@@ -82,8 +86,8 @@ ax1.set_xlabel('Endowment $Q_{1}^{2}$')
 ax1.set_ylabel('Consumption ')
 #fig4.savefig('C:/aaaCourses/AdvMacro/aaaHomeworks/2017/Python/fig4_C_Q12.png')   # save the figure to file
 
-
 # PLOT 3: VARIATION OF PREFERENCE PARAMETER RHO for Agent Type 1 in period 2
+
 r_3   =   r[0,0,0,0,:,0]
 C11_3 = C11[0,0,0,0,:,0]
 C12_3 = C12[0,0,0,0,:,0]
@@ -105,4 +109,5 @@ ax1.plot(rho1,C22_3,'k--',color='b',label='$C_{2}^{2}$')
 ax1.legend(loc='best')
 ax1.set_xlabel(r'Discount rate $\rho_{1}$')
 ax1.set_ylabel('Consumption ')
-#fig6.savefig('C:/aaaCourses/AdvMacro/aaaHomeworks/2017/Python/fig6_C_rho1.png')   # save the figure to file            
+#fig6.savefig('C:/aaaCourses/AdvMacro/aaaHomeworks/2017/Python/fig6_C_rho1.png')   # save the figure to file
+            
